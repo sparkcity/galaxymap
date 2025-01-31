bd1a3a1aeb0e31e7d99ffffe280d52ec6ca01aa6
Parker <23155686+sparkcity@users.noreply.github.com>
2025-01-30 23:51:29 -0500

    update

--- main/js/map.js
+++ main/js/map.js
@@ -1,13 +1,18 @@
+////////// Create Map Box
 const map = L.map('map', {
     crs: L.CRS.Simple
 });
 
 const bounds = [[0, 0], [1000, 1000]];
-const image = L.imageOverlay('../src/img/galaxyspiral.jpg', bounds).addTo(map);
+const image = L.imageOverlay('../src/img/galaxyspiral2.png', bounds).addTo(map);
 
 map.fitBounds(bounds);
 
+////////// FUNCTION FOR FINDING COORDINATES
 function onMapClick(e){
     console.log(e.latlng)
 }
 map.on('click', onMapClick);
+
+////////// Coordinates
+
